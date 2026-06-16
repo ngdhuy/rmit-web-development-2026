0dbd2a4778235dba90cd198ad31bf3375b5026ac
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2026-06-16 03:31:30 +0000

    feat(session03): create Footer (function) component

--- session-03-react/lab-01/src/App.tsx
+++ session-03-react/lab-01/src/App.tsx
@@ -3,17 +3,20 @@ import React from 'react'
 
 // import project component
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 // import css file
 import './App.css'
 
 // define function component with type Rreact.FC
 const App : React.FC = () => {
-  
+  const message = "Designed by SEstudio"
+
   // function component must be return HTML tag with root is single tag
   return (
     <>
       <Header title='Welcome to React applcation' />
+      <Footer message={message} />
     </>
   )
 }
---
+++ session-03-react/lab-01/src/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+/**
+ * * define function component for footeer of page
+ */
+
+import React from "react"
+
+import './style.css'
+
+type Props = { message : string }
+
+const Footer : React.FC<Props> = (props : Props) => {
+    return (
+        <footer>
+            <h3>{ props.message }</h3>
+        </footer>
+    )
+}
+
+export default Footer
