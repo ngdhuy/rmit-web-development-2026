e8cf2e220ddece38440972d7d214eee747fb5c93
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2026-06-23 14:49:55 +0000

    feat(session03): create NavBar component with react-router-dom

--- session-03-react/lab-02/src/pages/index.tsx
+++ session-03-react/lab-02/src/pages/index.tsx
@@ -3,11 +3,21 @@ import React from "react"
 
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import NavBar from "../components/NavBar"
+
+import { BrowserRouter, Routes } from "react-router-dom"
 
 const Pages: React.FC = () => {
     return (
         <>
             <Header />
+            <BrowserRouter>
+                <NavBar />
+                <Routes>
+
+                </Routes>
+            </BrowserRouter>
+            
             <Footer />
         </>
     )
