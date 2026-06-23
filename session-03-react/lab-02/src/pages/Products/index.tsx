b93a1e4468a563fcc335458cbde8af9afd1b6bd6
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2026-06-23 15:32:07 +0000

    feat(session03): create component Create new Product

--- session-03-react/lab-02/src/pages/Products/index.tsx
+++ session-03-react/lab-02/src/pages/Products/index.tsx
@@ -1,17 +1,20 @@
 import type React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 import './style.css'
 import ListOfProduct from "./ListOfProduct";
 import DetailOfProduct from "./DetailOfProduct";
+import CreateProduct from "./CreateProduct";
 
 const Products : React.FC = () => {
     return(
         <div className='product'>
             <h2>Product management</h2>
+            <Link to="new">Create new Product</Link>
             <Routes>
                 <Route index element={ <ListOfProduct /> } />
                 <Route path=":id" element={ <DetailOfProduct /> } />
+                <Route path="new" element={ <CreateProduct /> } />
             </Routes>
         </div>
     )
