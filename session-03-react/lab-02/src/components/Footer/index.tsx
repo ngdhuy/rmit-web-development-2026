1bb3f9912fc58b928e4cb3b91abfec98a92a9c73
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2026-06-23 14:23:59 +0000

    feat(session03): create Footer component

---
+++ session-03-react/lab-02/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+// import component
+import React from "react"
+
+// import css file
+import './style.css'
+
+const Footer : React.FC = () => {
+    return (
+        <div className='footer'>
+            Design by ndhuy - &copy; 2026
+        </div>
+    )
+}
+
+export default Footer
--- session-03-react/lab-02/src/pages/index.tsx
+++ session-03-react/lab-02/src/pages/index.tsx
@@ -2,11 +2,13 @@
 import React from "react"
 
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 const Pages: React.FC = () => {
     return (
         <>
             <Header />
+            <Footer />
         </>
     )
 }
