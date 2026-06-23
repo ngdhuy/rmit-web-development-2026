0aaa66f62676bb22461af40442e3dcd63e807dec
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2026-06-23 14:07:17 +0000

    feat(session03): init react project

---
+++ session-03-react/lab-02/src/App.tsx
@@ -0,0 +1,15 @@
+// import component
+import React from 'react'
+
+// import css file
+import './App.css'
+
+// define function conponent for App
+const App : React.FC = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
