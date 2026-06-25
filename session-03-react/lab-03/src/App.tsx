2660760f0ad2e3ebc52d8388b863574ebcf92bd8
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2026-06-25 08:27:24 +0000

    feat(session03): add all Component to App

--- session-03-react/lab-03/src/App.tsx
+++ session-03-react/lab-03/src/App.tsx
@@ -1,13 +1,21 @@
 // impoort React component
 import type React from 'react'
+import { Toaster } from 'react-hot-toast'
+
+// import components
+import { AddTodo } from './components/AddTodo'
+import { TodoList } from './components/TodoList'
 
 // import SASS file
 import './App.sass'
 
 const App : React.FC = () => {
   return(
-    <>
-    </>
+    <div>
+        <Toaster position="bottom-center" />
+        <AddTodo />
+        <TodoList />
+    </div>
   )
 }
 
